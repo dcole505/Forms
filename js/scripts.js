2045dc3ae02e6dc9943cec2ad90c24eb4e2101f8
David Cole <davidtcole@yahoo.com>
2020-06-08 17:44:58 -0700

    add a forEach loop into our mad-libs program to display the input entered by the user in an unordered list before displaying the full mad-lib story.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,14 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
+    event.preventDefault();
+
     const person1Input = $("input#person1").val();
     const person2Input = $("input#person2").val();
     const animalInput = $("input#animal").val();
     const exclamationInput = $("input#exclamation").val();
     const verbInput = $("input#verb").val();
     const nounInput = $("input#noun").val();
+    const inputArray = [person1Input, person2Input, animalInput, exclamationInput, verbInput, nounInput];
 
     $(".person1").text(person1Input);
     $(".person2").text(person2Input);
@@ -14,9 +17,12 @@ $(document).ready(function() {
     $(".verb").text(verbInput);
     $(".noun").text(nounInput);
 
+    inputArray.forEach(function(element){
+      $("#display-input").append("<li>" + element + "</li>");
+    });
     $("#story").show();
 
-    event.preventDefault();
+
   });
 
   $("#formLetter").submit(function(event){
